323789fde08ba0ed7dd647b97313449ca782d24c
Win Steveker <win.steveker@gmx.de>
2025-05-02 08:58:13 -0400

    ruff test

--- run_from_root.py
+++
@@ -1,6 +0,0 @@
-# from utils.testing.create_tests import main as create_tests
-from utils.testing.run_tests import main as run_tests
-
-if __name__ == "__main__":
-    # create_tests()
-    run_tests()
