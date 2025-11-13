16294a2e33747036042d5f3e775c25c6582a7380
Winipedia <win.steveker@gmx.de>
2025-11-13 14:45:25 +0000

    init configs order fixed

--- winipedia_utils/dev/configs/builder.py
+++ winipedia_utils/dev/configs/builder.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 
 import winipedia_utils
+from winipedia_utils.dev.artifacts import builder
 from winipedia_utils.dev.configs.base.base import PythonConfigFile
 from winipedia_utils.dev.configs.pyproject import PyprojectConfigFile
 from winipedia_utils.utils.modules.module import to_path
@@ -14,10 +15,6 @@ class BuilderConfigFile(PythonConfigFile):
     @classmethod
     def get_parent_path(cls) -> Path:
         """Get the path to the config file."""
-        # avoids loading configs pkg when init_config_files is called
-        # bc it leads to import error in sub pkg when adding new dev deps
-        from winipedia_utils.dev.artifacts import builder  # noqa: PLC0415
-
         src_package = PyprojectConfigFile.get_package_name()
         builds_package = builder.__name__.replace(
             winipedia_utils.__name__, src_package, 1
